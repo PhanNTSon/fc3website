f93156dc920ab25efeede439a8b3c0e91ec4f72d
Phan Son <minikie456@gmail.com>
2025-02-12 15:18:23 +0700

    Done HTML and CSS Base

--- R-assets/js/main.js
+++ R-assets/js/main.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
-    $(".nav-content-tab").click(function(){
-        $("#department-tabs-detail article").hide();
-        var target = $(this).data("target");
-        $("#"+target).show();
+(function ($) {
+    $(document).ready(function () {
+        $(".nav-content-tab").click(function () {
+            $("#department-tabs-detail article").hide();
+            var target = $(this).data("target");
+            $("#" + target).show();
+        });
     });
-});
+    // Page loading animation
+    $(window).on('load', function () {
+
+        $('#js-preloader').addClass('loaded');
+
+    });
+})(window.jQuery);
