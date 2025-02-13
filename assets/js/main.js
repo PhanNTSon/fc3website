627b82542ae846ecfdb8acb182b83041ec74642d
Phan Son <minikie456@gmail.com>
2025-02-13 22:14:00 +0700

    WIP: Researching Navbar in bootstrap 5

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".nav-content-tab").click(function(){
-        $("#department-tabs-detail article").hide();
-        var target = $(this).data("target");
-        $("#"+target).show();
-    });
-});
